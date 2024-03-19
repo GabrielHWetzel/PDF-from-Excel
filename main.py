3e68095c1460bcf288f6207674ca85b74373d8b7
Gabriel Henrique Wetzel <gabrielhwetzel@gmail.com>
2024-03-19 17:17:05 -0300

    Loading Excel files from directory

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-import pandas
+import pandas as pd
+import glob
 
+invoices = glob.glob("invoices/*.xlsx")
+
+for invoice in invoices:
+    df = pd.read_excel(invoice, sheet_name="Sheet 1")
+    print(df)
